b1af7999efdef15d7f69600545e94e69c0d52b8f
issuh <yoyus5@naver.com>
2020-06-09 23:25:12 +0900

    Test

--- main.cpp
+++ main.cpp
@@ -22,27 +22,75 @@ class FunctionMap {
 
   template<typename F>
   void registFunction(const std::string& name, F func) {
-    m_functionMap.insert(
-        td::make_pair(
-          name, func));
+    // m_functionMap.insert(
+    //     td::make_pair(
+    //       name, func));
   }
 
   void callFunction(const std::string& name) {}
 
- private:
-  std::map<std::string, std::function<()>> m_functionMap;
+//  private:
+  // std::map<std::string, std::function<()>> m_functionMap;
 };
 
+namespace helper {
+    template <int... Is>
+    struct index {};
+
+    template <int N, int... Is>
+    struct gen_seq : gen_seq<N - 1, N - 1, Is...> {};
+
+    template <int... Is>
+    struct gen_seq<0, Is...> : index<Is...> {};
+}
+
+template <typename R, typename... Ts>
+class Action {
+ private:
+  std::function<R(Ts...)> f;
+  std::tuple<Ts...> args;
+ public:
+    template <typename F, typename... Args>
+    Action(F&& func, Args&&... args) : f(std::forward<F>(func)),
+                                       args(std::forward<Args>(args)...) {}
+
+    template <typename... Args, int... Is>
+    void func(std::tuple<Args...>& tup, helper::index<Is...>) {
+        f(std::get<Is>(tup)...);
+    }
+
+    template <typename... Args>
+    void func(std::tuple<Args...>& tup) {
+        func(tup, helper::gen_seq<sizeof...(Args)>{});
+    }
+
+    void act() {
+        func(args);
+    }
+
+    int argCount() const { args.size(); }
+};
+
+template <typename F, typename... Args>
+Action<Args...> make_action(F&& f, Args&&... args) {
+    return Action<Args...>(std::forward<F>(f), std::forward<Args>(args)...);
+}
+
 int main() {
-  FunctionMap funcMap;
+  // FunctionMap funcMap;
 
-  funcMap.registFunction("HelloWorld", HelloWorld);
-  funcMap.registFunction("printArg", printArg);
-  funcMap.registFunction("sum", sum);
+  // funcMap.registFunction("HelloWorld", HelloWorld);
+  // funcMap.registFunction("printArg", printArg);
+  // funcMap.registFunction("sum", sum);
 
-  funcMap["HelloWorld"]();
-  funcMap["printArg"]("Hello!!");
-  funcMap["sum"](1, 3);
+  // funcMap["HelloWorld"]();
+  // funcMap["printArg"]("Hello!!");
+  // funcMap["sum"](1, 3);
 
+    auto add = make_action([](int a, int b, float c){ std::cout << a + b + c; }, 2, 2, 3.5);
+    auto sub = make_action([](int a, int b){ std::cout << a - b; }, 2, 2);
+
+    add.act();
+    sub.act();
   return 0;
 }
